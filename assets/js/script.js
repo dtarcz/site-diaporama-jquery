875884a3f741b1439555b8daf7b2bce3de9f5767
simplon-davidt <dtarcz@gmail.com>
2015-12-15 16:23:19 +0100

    commit initial

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // on exécute la fonction diaporama sur tous les <ul> qui ont une classe .diaporama
+    $("ul.diaporama").diaporama({
+        animationSpeed: "slow",
+        delay:2
+    });
+
+});
